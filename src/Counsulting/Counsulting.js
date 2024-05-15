3dc2ec576eb8ae094271677f2df9dc868c12b8b6
SiddAgilewit <siddhu@agilewits.com>
2024-05-15 18:10:17 +0530

    Cunsulting Page

--- src/Counsulting/Counsulting.js
+++ src/Counsulting/Counsulting.js
@@ -1,7 +1,25 @@
-const Counsulting=()=>{
-    return(
-        <div>
+import Header from '../Header/Header'
+import DeskTopHeader  from '../DeskTopHeader/DeskTopHeader'
+import BottomPage from '../BottomPage/BottomPage'
+import Options from '../Assets/Options.svg'
+import Chaticon from '../Assets/Chaticon.svg'
+import "./Counsulting.css"
+const Counsulting = () => {
+    return (
+        <div className='HomeTopLayer'>
+        <Header />
+        <DeskTopHeader />
+        <div className="HomeSecondLayer">
+        <img src={Options} alt="Options Icons" className="HomeOptionsIcon" />
+        <div className="SubHomeSecondLayer">
+            <BottomPage/>
+            <BottomPage/>
+            <BottomPage/>
+            <BottomPage/>
 
+        </div> 
+        <img src={Chaticon} alt="Chat" className="HomeOptionsIcon"/> 
+        </div>  
         </div>
     )
 }
